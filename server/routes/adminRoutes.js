0a7195a316bb6fa8069ad6fe2b4a5cf3e0de2a4e
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-03-23 13:53:59 +0530

    Add files via upload

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,9 @@
+// routes/adminRoutes.js
+const express = require('express');
+const router = express.Router();
+const { getAdminStats } = require("../controllers/adminController.js");
+const { protect, admin } = require("../middleware/authMiddleware.js");
+
+router.get("/stats", protect, admin, getAdminStats); 
+
+module.exports = router; 
