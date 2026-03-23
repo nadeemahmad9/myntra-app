b84a54cf32389643bf47dacf91a7f6b9a547043e
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-03-23 13:54:44 +0530

    Delete server/routes/admin.js

--- server/routes/admin.js
+++
@@ -1,9 +0,0 @@
-// routes/adminRoutes.js
-const express = require("express")
-const router = express.Router()
-const { getAdminStats } = require("../controllers/adminController")
-const { protect, admin } = require("../middleware/authMiddleware")
-
-router.get("/stats", protect, admin, getAdminStats)
-
-module.exports = router
