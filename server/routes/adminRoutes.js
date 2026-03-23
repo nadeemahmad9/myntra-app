fa25c2ce0d238159834d2d01f127d2c0cdac9268
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-03-23 14:06:03 +0530

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,9 +1,12 @@
 // routes/adminRoutes.js
-const express = require('express');
+import express from 'express';
 const router = express.Router();
-const { getAdminStats } = require("../controllers/adminController.js");
-const { protect, admin } = require("../middleware/authMiddleware.js");
 
+// Note: Controllers aur Middleware ke aage .js extension zaroori hai
+import { getAdminStats } from "../controllers/adminController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+// Route definition
 router.get("/stats", protect, admin, getAdminStats); 
 
-export default router; 
+export default router;
