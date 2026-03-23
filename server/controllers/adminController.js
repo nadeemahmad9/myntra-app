cbaf4de17306ae01759d4e86f8c9532e75000223
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-03-23 15:43:19 +0530

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,8 +1,8 @@
 // controllers/adminController.js
-const asyncHandler = require("express-async-handler")
-const Order = require("../models/orderModel")
-const Product = require("../models/productModel")
-const User = require("../models/userModel")
+import asyncHandler from "express-async-handler";
+import Order from "../models/orderModel.js";
+import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 
 // GET /api/admin/stats
 // Private/Admin
