52461dba807480a2cd2e93ac3d4941710bc95333
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-03-29 10:00:21 +0530

    Update adminRoutes.js

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -7,6 +7,6 @@ import { getAdminStats, createProduct } from "../controllers/adminController.js"
 import { protect } from "../middleware/authMiddleware.js";
 
 // Route definition
-router.get("/stats",protect,admin, getAdminStats); 
+router.get("/stats",protect, getAdminStats); 
 router.post("/products",protect, createProduct); 
 export default router;
