175c5a545503cdab072529bf10a5bfb84305e0f8
Nadeem Ahmad <127886628+nadeemahmad9@users.noreply.github.com>
2026-04-04 11:37:24 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -132,6 +132,7 @@ import axios from "axios";
 
 const api = axios.create({
      baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+     withCredentials: true, // 👈
 
   headers: {
     "Content-Type": "application/json",
